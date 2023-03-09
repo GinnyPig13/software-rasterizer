f56b1a8c0cfba6bcb61950a922fa4b957addbb54
GinnyPig13 <ginny.caynak@gmail.com>
2023-03-09 02:22:15 -0800

    Created full screen mode

--- src/main.c
+++ src/main.c
@@ -17,6 +17,13 @@ bool initialize_window(void) {
 		fprintf(stderr, "Error initializing SDL.\n");
 		return false;
 	}
+
+	SDL_DisplayMode display_mode;
+	SDL_GetCurrentDisplayMode(0, &display_mode);
+
+	window_width = display_mode.w;
+	window_height = display_mode.h;
+
 	//Create a SDL Window
 	window = SDL_CreateWindow(
 		NULL,
